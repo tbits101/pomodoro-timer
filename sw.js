994179985b3bacd2ef53090a933fc1b1eed5c629
Tbits101 <tbits101@gmail.com>
2026-01-20 17:25:56 +0100

    Convert to PWA: add manifest, sw, and icons

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pomodoro-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './version.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+    'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@300;600&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Strategy: Cache First, fallback to Network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
